891c4b0ef64de2a65747b0c7c267ac637057546d
ShindeYash <yashpradeepshinde@gmail.com>
2024-12-06 22:03:22 +0530

    Adding all my learning files

---
+++ 2.7 Literals.java
@@ -0,0 +1,38 @@
+public class Demo {
+
+	public static void main(String[] args) {
+
+	// Binary Number
+	int num1=0b101;
+	System.out.println(num1);
+
+	// HexaDecimal Number
+	int num2=0x7E;
+	System.out.println(num2);
+
+	// Comma Numbers for easy readability
+	int num3=10_00_00_000;
+	System.out.println(num3);
+	
+	float num4=56;
+	System.out.println(num4);
+	
+	double num5=56;
+	System.out.println(num5);
+	
+	double num6=12e10;
+	System.out.println(num6);
+	
+	boolean num7= true;
+	System.out.println(num7);
+	
+	char c='a';
+	System.out.println(c);
+	c++;
+	
+	char c1='a';
+	c1++;
+	System.out.println(c1);
+	}
+
+}
